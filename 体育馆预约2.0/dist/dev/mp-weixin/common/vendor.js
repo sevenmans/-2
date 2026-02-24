45dd33660b4fb697237a595022b225f548191f8a
lianghengpan <elephant00007777@gmail.com>
2026-02-24 15:45:34 +0800

    chore: 更新小程序开发构建的运行时标识符
    更新 vendor.js 中的运行时 socket 服务标识符，以反映最新的构建状态

--- 体育馆预约2.0/dist/dev/mp-weixin/common/vendor.js
+++ 体育馆预约2.0/dist/dev/mp-weixin/common/vendor.js
@@ -7060,7 +7060,7 @@ function isConsoleWritable() {
 function initRuntimeSocketService() {
   const hosts = "127.0.0.1,192.168.3.5";
   const port = "8090";
-  const id = "mp-weixin_RzW8uC";
+  const id = "mp-weixin_Ggea1a";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
